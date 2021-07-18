920bab3b4537946417cceb2f831cdb00f18602ff
Ciro/Coda <ciro.carandente@protonmail.com>
2021-07-18 12:40:30 +0200

    feat: added react-native-vector-icons (missing iOS support, need a mac)

--- src/navigation.tsx
+++ src/navigation.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { NavigationContainer } from "@react-navigation/native";
+import {NavigationContainer} from '@react-navigation/native';
 
 interface IProps {}
 
 const Navigation: React.FC<IProps> = () => {
-  return <NavigationContainer></NavigationContainer>;
+  return <></>; // <NavigationContainer></NavigationContainer>;
 };
 
 export default Navigation;
