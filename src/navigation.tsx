580c0f4f9b0fd6dc99871b1df3281d65e2e6696b
Ciro/Coda <ciro.carandente@protonmail.com>
2021-07-18 12:23:59 +0200

    feat: added react navigation

---
+++ src/navigation.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavigationContainer } from "@react-navigation/native";
+
+interface IProps {}
+
+const Navigation: React.FC<IProps> = () => {
+  return <NavigationContainer></NavigationContainer>;
+};
+
+export default Navigation;
